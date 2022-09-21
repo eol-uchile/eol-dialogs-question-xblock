db892c37a1ebab66216b264f1ce238d3c252dd7f
Diego Higuera <dhiguerafernandez@gmail.com>
2022-09-21 18:14:06 -0300

    Fix multiple input answer

--- dialogsquestionsxblock/static/js/src/dialogsq.js
+++ dialogsquestionsxblock/static/js/src/dialogsq.js
@@ -96,14 +96,28 @@ function DialogsQuestionsXBlock(runtime, element, settings) {
         $obj.find('.showing_answers').remove();
             $.each($obj.find(".inputdialogo"), function(j,v){
                 $img = $cruz;
-                if(result.answers[$(v).attr('question-id')] == $(v).val())
-                    $img = $ticket;
-                $(v).after('<span class="showing_answers"> &nbsp;'+$img+'&nbsp;<strong>Respuesta:</strong> '+ result.answers[$(v).attr('question-id')].replace("[_[i]_]","") +'&nbsp; </span>');
+                
+                ans_inputs = ((result.answers[$(v).attr('question-id')]).replace("[_[i]_]","")).split("_")
+                input_resp = ""
+
+                ans_inputs.forEach(
+                    function(ans){
+                        if(ans == $(v).val) {
+                            $img = $ticket;
+                        }
+                        input_resp += ans + " o "
+                    }
+                );
+
+                input_resp += "+_punto*"
+                input_resp.replace("o +_punto*",".")
+
+                $(v).after('<span class="showing_answers"> &nbsp;'+$img+'&nbsp;<strong>Respuesta:</strong> '+ input_resp +'&nbsp; </span>');
             })
             $.each($obj.find(".dropdowndialogo"), function(j,v){
                 $img = $cruz;
                 console.log(result.answers[$(v).attr('question-id')]+" "+$(v).val());
-                if(result.answers[$(v).attr('question-id')] == $(v).val())
+                if ( (result.answers[$(v).attr('question-id')]).replace("[_[s]_]","") == $(v).val())
                     $img = $ticket;
                 $(v).after('<span class="showing_answers"> &nbsp;'+$img+'&nbsp;<strong>Respuesta:</strong> '+ result.answers[$(v).attr('question-id')].replace("[_[s]_]","") +'&nbsp; </span>');
             })
