75b8f1dc8ef8088de849d576f924d52b6617b0a6
Diego Leonardo Higuera Fernandez <lozamded@pop-os.localdomain>
2024-04-02 18:38:55 -0300

    FIX: mathjax in studentview

--- dialogsquestionsxblock/dialogsquestionsxblock.py
+++ dialogsquestionsxblock/dialogsquestionsxblock.py
@@ -179,6 +179,7 @@ class DialogsQuestionsXBlock(StudioEditableXBlockMixin, XBlock):
         frag = Fragment(template)
         frag.add_css(self.resource_string("static/css/dialogsq.css"))
         frag.add_javascript(self.resource_string("static/js/src/utils.js"))
+        frag.add_javascript(self.resource_string("static/js/src/mathjax.js"))
         frag.add_javascript(self.resource_string("static/js/src/dialogsq.js"))
         settings = {
             'image_path': self.runtime.local_resource_url(self, 'public/images/')
@@ -207,7 +208,6 @@ class DialogsQuestionsXBlock(StudioEditableXBlockMixin, XBlock):
         template = self.render_template('static/html/studio_edit.html', context)
         fragment = Fragment(template)
         fragment.add_javascript(self.resource_string("static/js/src/utils.js"))
-        fragment.add_javascript(self.resource_string("static/js/src/mathjax.js"))
         fragment.add_javascript(self.resource_string("static/js/src/studio_edit.js"))
         fragment.initialize_js('StudioEditableXBlockMixin')
         return fragment
